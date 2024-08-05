c7f618c69252ccc98cf0084ce9a6489685d44ee5
sathishkumar6233 <sathishkutta59@gmail.com>
2024-08-05 19:49:07 +0530

    add-cart

---
+++ src/components/footer.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect} from 'react' 
+import { Link } from 'react-router-dom'
+import iconCart from '../assets/images/iconCart.png' 
+import { useSelector, useDispatch } from 'react-redux' 
+import { toggleStatusTab } from '../stores/cart'
+
+const Footer = () => {
+    return (
+        <>
+            <div className="Footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-lg-5 col-12 ft-1">
+                            <h3><span></span>CODES</h3>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum ea quo ex ullam laboriosam magni totam, facere eos iure voluptate.</p>
+                            <div className="footer-icons">
+                                <i class="fa-brands fa-facebook"></i>
+                                <i class="fa-brands fa-twitter"></i>
+                                <i class="fa-brands fa-instagram"></i>
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-lg-3 col-12 ft-2">
+                            <h5>Quick Links</h5>
+                            <ul>
+                                <li className="nav-item">
+                                    <a className="" href="/">Services</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">Portfolio</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">Contact Us</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">Services</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="" href="/">Portfolio</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-md-6 col-lg-4 col-12 ft-3">
+                            <h5>Quick Links</h5>
+                            <p><i class="fa-solid fa-phone-volume"></i> +92 3121324083</p>
+                            <p><i class="fa-solid fa-envelope"></i> sathishkumar@gmail.com</p>
+                            <p><i class="fa-solid fa-paper-plane"></i> k.k nagar, madurai.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='Last-footer'>
+                <p>sathish kumar</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
