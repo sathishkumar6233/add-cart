e07d262418b12a6de2a2b5c69805cbddc7046818
sathishkumar6233 <168398061+sathishkumar6233@users.noreply.github.com>
2024-08-12 11:36:38 +0530

    Update cartItem.js

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -27,7 +27,7 @@ const CartItem = (props) => {
     <div className='flex justify-between items-center bg-slate-600 text-white p-2 border-b-2 border-slate-700 gap-5 rounded-md'>
         <img src={detail.image} alt="" className='w-12'/>
         <h3>{detail.name}</h3>
-        <p>${detail.price * quantity}</p>
+        <p>₹{detail.price * quantity}</p>
         <div className='w-20 flex justify-between gap-2'>
             <button className='bg-gray-200 rounded-full w-6 h-6 text-cyan-600' onClick={handleMinusQuantity}>-</button>
             <span>{quantity}</span>
